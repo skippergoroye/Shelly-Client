ca57d55956dbb9139f026c13e060ca9a948662c7
skippergoroye <oluwatosinadegoroye21@gmail.com>
2026-06-20 09:24:42 +0100

    feat: prettier addedjdsjdsjjds jjjsjsjjsjs hhhh

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
     <div>
-      
+      dhhdhdhsdhhdshsdhds
+      <p>jsdjsdjjdsjjsdjds</p>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
